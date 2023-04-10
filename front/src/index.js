efd1b1e41a83e6bbd7fdcb76f27bea0dc8ed0f71
Julio Dalmau <104108635+JulioDalmau@users.noreply.github.com>
2023-04-09 23:25:56 -0300

    Add colors MUI

--- front/src/index.js
+++ front/src/index.js
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter} from "react-router-dom";
-
+import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "@emotion/react";
+import { theme } from "./theme";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </BrowserRouter>
 );
